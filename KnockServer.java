134e80e7042e4d329db4394ebb5c3d95e2005f25
markeatconcord26 <ellisonm26@mycu.concord.edu>
2014-04-09 10:27:53 -0400

    Update KnockServer.java

--- KnockServer.java
+++ KnockServer.java
@@ -4,7 +4,10 @@ import java.net.*;
 
 
 public class KnockServer {
+    
 
+    //Arrays that hold the Answers and Jokes of the 
+    //Knock knock sender.
     String[] AnsList = {"Canoe","Merry","Orange"};
     String[] JokeList = {"Canoe help me with my homework?","Merry Christmas!"
     ,"Orange you going to let me in?"};
@@ -15,6 +18,8 @@ public class KnockServer {
         {
             ServerSocket connection = new ServerSocket (4242);
             
+            //A loop that contualliy links up with the client.
+            //Prints nessary Answer and Jokes to the  client.
             while(true)
             {
                 Socket Sock = connection.accept();
@@ -36,15 +41,24 @@ public class KnockServer {
             ex.printStackTrace();
         }
     }
+    
+    //Main function. Creates the thread that
+    //runs the server.
     public static void main(String[] args) 
     {
         KnockServer Server = new KnockServer();
         Server.go();
     }
+    
+    //Gets one of the elements from the joke list
+    //in accordance to the random integer that was picked.
     private String getJoke(int Postion)
     {
         return JokeList[Postion];
     }
+    
+    //Gets one of the elements from the Anwser list
+    //in accordance to the random integer that was picked.
     private String getAns(int Postion)
     {
         return AnsList[Postion];
